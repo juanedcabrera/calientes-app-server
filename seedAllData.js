588a1a3b1bb4032f6cffcef35900c9ecc6310745
Andrew <andrew@andrewsmith.org>
2023-05-23 04:41:05 -0400

    Seed seasons and episodes with proper reference IDs

---
+++ seedAllData.js
@@ -0,0 +1,91 @@
+// filename "seedAllData" signifies that the logic here is designed specifically for starting with a fresh/blank db and populating a new environment with ALL needed data.  for this project, it may be useful to also have a "seedNewData" script that can be run periodically to add only the most recent episode and guest, etc.  but in practice, it's possible that the logic will end up being nearly identical and we'll determine the distinction to be redundant.
+
+const db = require('./models');
+const { data } = require('./data');
+
+const checkSeasonOfEpisode = (episodeNumber) => {
+  if (episodeNumber < 9) {
+    return 1;
+  }
+  if (episodeNumber < 48) {
+    return 2;
+  }
+  if (episodeNumber < 72) {
+    return 3;
+  }
+  if (episodeNumber < 94) {
+    return 4;
+  }
+  if (episodeNumber < 110) {
+    return 5;
+  }
+  if (episodeNumber < 123) {
+    return 6;
+  }
+  if (episodeNumber < 135) {
+    return 7;
+  }
+  if (episodeNumber < 147) {
+    return 8;
+  }
+  if (episodeNumber < 159) {
+    return 9;
+  }
+  if (episodeNumber < 171) {
+    return 10;
+  }
+  if (episodeNumber < 180) {
+    return 11;
+  }
+  if (episodeNumber < 190) {
+    return 12;
+  }
+  if (episodeNumber < 200) {
+    return 13;
+  }
+  if (episodeNumber < 212) {
+    return 14;
+  }
+  if (episodeNumber < 224) {
+    return 15;
+  }
+  if (episodeNumber < 237) {
+    return 16;
+  }
+  if (episodeNumber < 249) {
+    return 17;
+  }
+  if (episodeNumber < 261) {
+    return 18;
+  }
+  if (episodeNumber < 273) {
+    return 19;
+  }
+  if (episodeNumber < 285) {
+    return 20;
+  }
+};
+
+const populateDb = async () => {
+  data.seasons.map(async (season) => {
+    await db.Season.create(season);
+  });
+  data.episodes.map(async (episode, index) => {
+    const newEpisode = await db.Episode.create(episode);
+    seasonNumber = checkSeasonOfEpisode(index + 1);
+    let [season] = await db.Season.find({ seasonNumber });
+    season.episodes.push(newEpisode._id);
+    // following line is particularly important because each episode is seeded with a dummy ObjectId on creation to avoid errors (it may eventually be preferable to instead change the schema so this field is not required at creation and then omit the dummy value).  if the season property is not correctly set during this function, episodes will have a value for season that does not actually point to anything in the seasons collection.
+    newEpisode.season = season._id;
+    await newEpisode.save();
+    await season.save();
+  });
+  //   data.guests.map(async (guest) => {
+  //     await db.Guest.create(guest);
+  //   });
+  //   data.sauces.map(async (sauce) => {
+  //     await db.Sauce.create(sauce);
+  //   });
+};
+
+populateDb();
